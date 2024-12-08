ec19a21a9103299869bb483b458a04b433b135c0
Isabelle Li <izzyli88@login-el8.khoury.northeastern.edu>
2024-12-08 02:19:59 -0500

    mkdir, cd, home updates

--- FileSys.cpp
+++ FileSys.cpp
@@ -23,7 +23,7 @@ void FileSys::unmount() {
 // make a directory
 void FileSys::mkdir(const char *name)
 {
-  // val directory name
+  // validate directory name
   if (strlen(name) == 0 || strlen(name) > MAX_FNAME_SIZE) {
     cerr << "Error: Invalid directory name given." << endl;
     return;
@@ -31,32 +31,64 @@ void FileSys::mkdir(const char *name)
 
   // check: directory exists
   dirblock_t curr_d_block;
-  bfs.read_block(curr_dir, (void*) & curr_d_block);
+  bfs.read_block(curr_dir, (void*) &curr_d_block);
 
-  for (int i = 0; i < new_dir.num_entries; i++) {
-    if (curr_dir[i].name == name) {
-      cerr << "Error: directory already exists." << endl;
+  for (int i = 0; i < curr_d_block.num_entries; i++) {
+    if (strcmp(name, curr_d_block.dir_entries[i].name) == 0) {
+      cerr << "Error: Directory already exists." << endl;
       return;
     }
   }
 
-  // find new block
-  short new_d_block = bfs.get_free_block();
-  if (new_d_block == 0) {
-    cerr << "Error: disk is full." << endl;
+  // check if curr dir has space for new dir
+  if (curr_d_block.num_entries == MAX_DIR_ENTRIES) {
+    cerr << "Error: Maximum number of directory entries reached." << endl;
     return;
   }
 
+  // find new block num
+  short new_block_num = bfs.get_free_block();
+  if (new_block_num == 0) {
+    cerr << "Error: Disk is full." << endl;
+    return;
+  }
 
+  // init new dir block
+  dirblock_t directory_block;
+  directory_block.magic = DIR_MAGIC_NUM;
+  directory_block.num_entries = 0;
+
+  // write new dir to disk
+  bfs.write_block(new_block_num, (void*) &directory_block);
+  
+  // add new dir to curr dir
+  int index = curr_d_block.num_entries;
+  strcpy(curr_d_block.dir_entries[index].name, name); 
+  curr_d_block.dir_entries[index].block_num = new_block_num;
+  curr_d_block.num_entries++;
+
+  // write curr dir to disk
+  bfs.write_block(curr_dir, (void*) &curr_d_block);
 }
 
 // switch to a directory
 void FileSys::cd(const char *name)
 {
+  dirblock_t curr_dir_block;    // get curr dir block
+  bfs.read_block(curr_dir, (void*) &curr_dir_block);
+
+  for (int i = 0; i < curr_dir_block.num_entries; i++) {  // iterate thru dir entries
+    if (strcmp(curr_dir_block.dir_entries[i].name, name) == 0) {    // dir name matches
+      curr_dir = curr_dir_block.dir_entries[i].block_num;
+      return;
+    }
+  }
+  cerr << "Error: Directory " <<  name << " not found." << endl;
 }
 
 // switch to home directory
 void FileSys::home() {
+  curr_dir = HOME_DIR;
 }
 
 // remove a directory
@@ -100,7 +132,7 @@ void FileSys::stat(const char *name)
 }
 
 // HELPER FUNCTIONS (optional)
-bool is_directory(short block_num) {
+bool FileSys::is_directory(short block_num) {
   dirblock_t block;
   bfs.read_block(block_num, (void*) &block);
 
